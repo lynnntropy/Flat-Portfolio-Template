3a0abcb22934f949068d41c5b517d66cbbd3a793
Veselin Romic <omegavesko@gmail.com>
2014-08-29 20:46:29 +0200

    Added existing files.

---
+++ index.php
@@ -0,0 +1,50 @@
+<html>
+
+    <head>
+        <meta charset="utf-8">
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
+        <script src="bower_components/platform/platform.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+
+        <link href='http://fonts.googleapis.com/css?family=Roboto:500,700,400,100,300' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="main.css">
+
+        <link rel="import" href="bower_components/paper-checkbox/paper-checkbox.html">
+        <link rel="stylesheet" href="bower_components/paper-shadow/paper-shadow.css">
+
+        <link rel="import" href="bower_components/paper-tabs/paper-tabs.html">
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="main-paper card paper-shadow-top-z-1">
+                <div class="card-inner paper-shadow-bottom-z-1">
+                    <div class="name-header">
+                        <div class="name">Veselin Romić</div>
+                        <div class="subtitle">Software Developer. UX Designer. Tinkerer.</div>
+
+                        <div id="header-tabs">
+                            <paper-tabs selected="0" class="transparent-teal">
+
+                                <paper-tab>ITEM ONE</paper-tab>
+                                <paper-tab>ITEM TWO</paper-tab>
+                                <paper-tab>ITEM THREE</paper-tab>
+
+                            </paper-tabs>
+                        </div>
+                    </div>
+
+
+
+                    <div class="test-center-page">
+                        <paper-checkbox></paper-checkbox>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+
+</html>
